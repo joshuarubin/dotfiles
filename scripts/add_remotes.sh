cba739df80b36720377504e2decdf4fd4b985488
Joshua Rubin <joshua@rubixconsulting.com>
2019-01-18 02:45:52 -0700

    cleanup
    Signed-off-by: Joshua Rubin <joshua@rubixconsulting.com>

---
+++ scripts/add_remotes.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+git remote add -f zim https://github.com/Eriner/zim
+git remote add -f zsh-history-substring-search https://github.com/zsh-users/zsh-history-substring-search
+git remote add -f zsh-completions https://github.com/zsh-users/zsh-completions
+git remote add -f zsh-syntax-highlighting https://github.com/zsh-users/zsh-syntax-highlighting
+git remote add -f zsh-prompt-pure https://github.com/sindresorhus/pure
+git remote add -f zsh-prompt-liquidprompt https://github.com/nojhan/liquidprompt
+git remote add -f zsh-prompt-lean https://github.com/miekg/lean
+git remote add -f zsh-autosuggestions https://github.com/zsh-users/zsh-autosuggestions
+git remote add -f dotvim https://github.com/joshuarubin/dotvim
+git remote add -f dotzsh https://github.com/joshuarubin/dotzsh
+git remote add -f tmux-tpm https://github.com/tmux-plugins/tpm
---
+++ scripts/pull_subtrees.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+git pull -s subtree zim master
+git pull -s subtree zsh-history-substring-search master
+git pull -s subtree zsh-completions master
+git pull -s subtree zsh-syntax-highlighting master
+git pull -s subtree zsh-prompt-pure master
+git pull -s subtree zsh-prompt-liquidprompt master
+git pull -s subtree zsh-prompt-lean master
+git pull -s subtree zsh-autosuggestions master
+git pull -s subtree dotvim master
+git pull -s subtree dotzsh master
+git pull -s subtree tmux-tpm master
